ae7d8c8131bdd17fb427f1d89e35c651009f8fed
NguyenQuocKhanh0 <108500182+NguyenQuocKhanh0@users.noreply.github.com>
2025-08-29 09:21:20 +0700

    Update run_finetune.sh

--- egs/zipvoice/run_finetune.sh
+++ egs/zipvoice/run_finetune.sh
@@ -131,7 +131,7 @@ if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
             --finetune 1 \
             --base-lr 0.0001 \
             --num-epochs 2 \
-            --save-every-n 2000 \
+            --save-every-n 1000 \
             --keep-last-k 5 \
             --max-duration 740 \
             --max-len ${max_len} \
