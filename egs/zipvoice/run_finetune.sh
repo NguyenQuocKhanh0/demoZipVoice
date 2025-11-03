513e9e1c76f94620c63d3c98e704a1d1040d05b1
NguyenQuocKhanh0 <108500182+NguyenQuocKhanh0@users.noreply.github.com>
2025-11-03 14:11:12 +0700

    Update run_finetune.sh

--- egs/zipvoice/run_finetune.sh
+++ egs/zipvoice/run_finetune.sh
@@ -129,7 +129,7 @@ if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
             --world-size 1 \
             --use-fp16 1 \
             --finetune 1 \
-            --base-lr 0.00007 \
+            --base-lr 0.000065 \
             --num-epochs 2 \
             --save-every-n 1000 \
             --keep-last-k 4 \
